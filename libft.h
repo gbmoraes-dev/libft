b7febaed46000b2102ac8a3963179a3cf5d8b13d
Gabriel Moraes Ribeiro <112049865+gbmoraes-dev@users.noreply.github.com>
2023-10-18 15:56:07 -0300

    feat: Update libft.h file :sparkles:

--- libft.h
+++ libft.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamoraes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/18 13:18:12 by gamoraes          #+#    #+#             */
+/*   Updated: 2023/10/18 15:52:04 by gamoraes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 # define LIBFT_H
 
@@ -5,6 +17,19 @@
 # include <stdlib.h>
 # include <stdio.h>
 
-
+void	ft_bzero(void *string, size_t length);
+int		ft_isalnum(int character);
+int		ft_isalpha(int character);
+int		ft_isascii(int character);
+int		ft_isdigit(int character);
+int		ft_isprint(int character);
+void	*ft_memcpy(void *dest, const void *src, size_t length);
+void	*ft_memmove(void *dest, const void *src, size_t length);
+void	*ft_memset(void *string, int character, size_t length);
+size_t	ft_strlcpy(char *dest, const char *src, size_t length);
+size_t	ft_strlen(const char *string);
+int		ft_strncmp(const char *string1, const char *string2, size_t length);
+int		ft_tolower(int character);
+int		ft_toupper(int character);
 
 #endif

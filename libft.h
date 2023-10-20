d46a66fb7ddf86a57efd849df0e1f2598c5459cb
Gabriel Moraes Ribeiro <112049865+gbmoraes-dev@users.noreply.github.com>
2023-10-20 18:24:59 -0300

    chore: Update libft.h file :sparkles:

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gamoraes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 13:18:12 by gamoraes          #+#    #+#             */
-/*   Updated: 2023/10/18 15:52:04 by gamoraes         ###   ########.fr       */
+/*   Updated: 2023/10/20 13:17:33 by gamoraes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,10 @@ int		ft_isprint(int character);
 void	*ft_memcpy(void *dest, const void *src, size_t length);
 void	*ft_memmove(void *dest, const void *src, size_t length);
 void	*ft_memset(void *string, int character, size_t length);
+void	*ft_memchr(const void *string, int character, size_t length);
 size_t	ft_strlcpy(char *dest, const char *src, size_t length);
 size_t	ft_strlen(const char *string);
+char	*ft_strnstr(const char *big, const char *little, size_t length);
 char	*ft_strrchr(const char *string, int character);
 char	*ft_strchr(const char *string, int character);
 int		ft_strncmp(const char *string1, const char *string2, size_t length);

1ab0afb23088188bce07810386ab89132efbbbb0
Gabriel Moraes Ribeiro <112049865+gbmoraes-dev@users.noreply.github.com>
2023-10-23 17:44:51 -0300

    chore: Update libft.h file :sparkles:

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gamoraes <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 13:18:12 by gamoraes          #+#    #+#             */
-/*   Updated: 2023/10/20 13:17:33 by gamoraes         ###   ########.fr       */
+/*   Updated: 2023/10/23 17:24:41 by gamoraes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,23 @@
 
 # include <stddef.h>
 # include <stdlib.h>
-# include <stdio.h>
 
+int		ft_atoi(const char *nptr);
 void	ft_bzero(void *string, size_t length);
+void	*ft_calloc(size_t nmemb, size_t size);
 int		ft_isalnum(int character);
 int		ft_isalpha(int character);
 int		ft_isascii(int character);
 int		ft_isdigit(int character);
 int		ft_isprint(int character);
 void	*ft_memcpy(void *dest, const void *src, size_t length);
+int		ft_memcmp(const void *s1, const void *s2, size_t length);
 void	*ft_memmove(void *dest, const void *src, size_t length);
 void	*ft_memset(void *string, int character, size_t length);
+size_t	ft_strlcat(char *dest, const char *src, size_t length);
 void	*ft_memchr(const void *string, int character, size_t length);
 size_t	ft_strlcpy(char *dest, const char *src, size_t length);
+char	*ft_strdup(const char *string);
 size_t	ft_strlen(const char *string);
 char	*ft_strnstr(const char *big, const char *little, size_t length);
 char	*ft_strrchr(const char *string, int character);

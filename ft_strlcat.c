7960300e195a20109c601446958145bc50a00698
Gabriel Moraes Ribeiro <112049865+gbmoraes-dev@users.noreply.github.com>
2023-10-24 19:08:47 -0300

    chore: Update strlcat function :sparkles:

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,11 +14,23 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t length)
 {
-	size_t	size;
+	size_t	dsize;
+	size_t	ssize;
+	size_t	i;
 
-	size = ft_strlen(dest);
-	while (*src && length-- > 0)
-		dest[size++] = *src++;
-	dest[size] = '\0';
-	return (ft_strlen(dest));
+	dsize = ft_strlen(dest);
+	ssize = ft_strlen(src);
+	i = 0;
+	if (length > dsize + 1)
+	{
+		while (src[i] && (dsize + 1 + i) < length)
+		{
+			dest[dsize + i] = src[i];
+			i++;
+		}
+	}
+	dest[dsize + i] = '\0';
+	if (length < dsize)
+		return (length + ssize);
+	return (dsize + ssize);
 }

06e7c9b399de801a759ccf6490b040de8734d44b
Gabriel Moraes Ribeiro <112049865+gbmoraes-dev@users.noreply.github.com>
2023-11-05 04:11:07 -0300

    chore: Update substr function ✨

--- ft_substr.c
+++ ft_substr.c
@@ -14,22 +14,23 @@
 
 char	*ft_substr(char const *string, unsigned int start, size_t length)
 {
-    size_t sourcelen;
-    size_t substrlen;
-    char *substr;
+	size_t	sourcelen;
+	size_t	substrlen;
+	char	*substr;
 
-    if (!string)
-        return (NULL);
-    sourcelen = ft_strlen(string);
-    substrlen = sourcelen - (size_t) start;
-    if (start > sourcelen)
-        return ((char *)ft_calloc(1, sizeof(char)));
-    if (length > sourcelen)
-        length = sourcelen - start + 1;
-    if (substrlen > length)
-        substrlen = length;
-    substr = (char *)ft_calloc(length + 1, sizeof(char));
-    if (!substr)
-        return (NULL);
-    return (substr);
+	if (!string)
+		return (NULL);
+	sourcelen = ft_strlen(string);
+	substrlen = sourcelen - (size_t) start;
+	if (start > sourcelen)
+		return ((char *)ft_calloc(1, sizeof(char)));
+	if (length > sourcelen)
+		length = sourcelen - start + 1;
+	if (substrlen > length)
+		substrlen = length;
+	substr = (char *)ft_calloc(substrlen + 1, sizeof(char));
+	if (!substr)
+		return (NULL);
+	ft_strlcpy(substr, (string + start), length + 1);
+	return (substr);
 }

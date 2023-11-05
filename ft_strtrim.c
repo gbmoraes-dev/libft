310805fe3b2c61143c08edeeee5bba342c49dedb
Gabriel Moraes Ribeiro <112049865+gbmoraes-dev@users.noreply.github.com>
2023-11-05 04:10:36 -0300

    feat: Create strtrim function ✨

---
+++ ft_strtrim.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamoraes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 04:02:57 by gamoraes          #+#    #+#             */
+/*   Updated: 2023/11/05 04:02:59 by gamoraes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	start;
+	size_t	end;
+	char	*strtrim;
+
+	start = 0;
+	end = ft_strlen(s1);
+	if (!s1 || !set)
+		return (NULL);
+	while (s1[start] && ft_strchr(set, s1[start]) != NULL)
+		start++;
+	while (end > start && ft_strchr(set, s1[end - 1]) != NULL)
+		end--;
+	strtrim = (char *)ft_calloc((end - start + 1), sizeof(char));
+	if (!strtrim)
+		return (NULL);
+	ft_strlcpy(strtrim, (s1 + start), (end - start + 1));
+	return (strtrim);
+}
